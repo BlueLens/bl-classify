0246936403d048c7b9cfa61cc06e266c0a450f4b
Youngbok Yoon <master@bluehack.net>
2017-12-27 21:41:22 +0900

    Query products from DB

--- main.py
+++ main.py
@@ -2,36 +2,18 @@ from __future__ import print_function
 import uuid
 
 import os
-from multiprocessing import Process
+import pickle
 
 import redis
 import time
-import pickle
 from bluelens_spawning_pool import spawning_pool
 from stylelens_product.products import Products
 
 from bluelens_log import Logging
 
 
-HOST_URL = 'host_url'
-TAG = 'tag'
-SUB_CATEGORY = 'sub_category'
-PRODUCT_NAME = 'product_name'
-IMAGE_URL = 'image'
-PRODUCT_PRICE = 'product_price'
-CURRENCY_UNIT = 'currency_unit'
-PRODUCT_URL = 'product_url'
-PRODUCT_NO = 'product_no'
-MAIN = 'main'
-NATION = 'nation'
-
-SPAWN_MAX = 10
-
-REDIS_HOST_CLASSIFY_QUEUE = 'bl:host:classify:queue'
 REDIS_PRODUCT_QUERY_QUEUE = 'bl:product:query:queue'
-REDIS_PRODUCT_CLASSIFY_BUFFER = 'bl:product:classify:buffer'
 REDIS_PRODUCT_CLASSIFY_QUEUE = 'bl:product:classify:queue'
-REDIS_PRODUCT_IMAGE_PROCESS_QUEUE = 'bl:product:image:process:queue'
 REDIS_CRAWL_VERSION = 'bl:crawl:version'
 REDIS_CRAWL_VERSION_LATEST = 'latest'
 
@@ -64,45 +46,6 @@ log = Logging(options, tag='bl-classify')
 
 product_api = None
 
-def get_latest_crawl_version():
-  value = rconn.hget(REDIS_CRAWL_VERSION, REDIS_CRAWL_VERSION_LATEST)
-  version_id = value.decode("utf-8")
-  return version_id
-
-def query(host_code):
-  global product_api
-  log.info('start query: ' + host_code)
-
-  version_id = get_latest_crawl_version()
-
-  q_offset = 0
-  q_limit = 100
-
-  try:
-    while True:
-      res = product_api.get_products_by_hostcode_and_version_id(host_code, version_id,
-                                                                is_processed=False,
-                                                                offset=q_offset, limit=q_limit)
-      for p in res:
-        push_product_to_queue(p)
-      if q_limit > len(res):
-        break
-      else:
-        q_offset = q_offset + q_limit
-  except Exception as e:
-    log.error(str(e) + ':' + host_code)
-
-def cleanup_products(host_code, version_id):
-  global product_api
-  try:
-    res = product_api.delete_products_by_hostcode_and_version_id(host_code, version_id,
-                                                              except_version=True)
-    log.debug(res)
-  except Exception as e:
-    log.error(e)
-
-def push_product_to_queue(product):
-  rconn.lpush(REDIS_PRODUCT_IMAGE_PROCESS_QUEUE, pickle.dumps(product))
 
 def spawn_classifier(uuid):
 
@@ -146,29 +89,42 @@ def spawn_classifier(uuid):
   pool.setRestartPolicy('Never')
   pool.spawn()
 
-def dispatch_query_job(rconn):
-  global product_api
+def get_latest_crawl_version():
+  value = rconn.hget(REDIS_CRAWL_VERSION, REDIS_CRAWL_VERSION_LATEST)
+  version_id = value.decode("utf-8")
+  return version_id
+
+
+def dispatch_classifier():
   product_api = Products()
-  while True:
-    key, value = rconn.blpop([REDIS_HOST_CLASSIFY_QUEUE])
-    query(value.decode('utf-8'))
+  offset = 0
+  limit = 500
+  version_id = get_latest_crawl_version()
 
-def dispatch_classifier(rconn):
+  try:
+    while True:
+      res = product_api.get_products_by_version_id(version_id=version_id,
+                                                   is_processed=True,
+                                                   is_classified=False,
+                                                   offset=offset,
+                                                   limit=limit)
 
-  count = 0
-  while True:
-    len = rconn.llen(REDIS_PRODUCT_CLASSIFY_QUEUE)
-    if len > 0 and count < SPAWN_MAX:
-      spawn_classifier(str(uuid.uuid4()))
-      count = count + 1
-    time.sleep(60 + count * 10)
-    # if len > 0:
-    #   spawn_classifier(str(uuid.uuid4()))
-    # time.sleep(300)
+      for product in res:
+        rconn.lpush(REDIS_PRODUCT_CLASSIFY_QUEUE, pickle.dumps(product))
+
+      if limit > len(res):
+        offset = 0
+        time.sleep(120)
+      else:
+        offset = offset + limit
+        spawn_classifier(str(uuid.uuid4()))
+        time.sleep(60)
+
+  except Exception as e:
+    log.error(str(e))
 
 if __name__ == '__main__':
   try:
-    Process(target=dispatch_query_job, args=(rconn,)).start()
-    Process(target=dispatch_classifier, args=(rconn,)).start()
+    dispatch_classifier()
   except Exception as e:
     log.error(str(e))
